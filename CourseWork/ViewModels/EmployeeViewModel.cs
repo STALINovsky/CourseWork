32cf12925ecf2a23d4c2777c61c1e676c633fdf8
STALINovsky <62829813+STALINovsky@users.noreply.github.com>
2022-05-22 16:10:08 +0300

    ............

--- CourseWork/ViewModels/EmployeeViewModel.cs
+++ CourseWork/ViewModels/EmployeeViewModel.cs
@@ -4,7 +4,9 @@ using DevExpress.Mvvm.DataAnnotations;
 using DevExpress.Mvvm.Utils;
 using DevExpress.Mvvm.Xpf;
 using Model;
+using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace CourseWork.ViewModels
 {
@@ -28,9 +30,45 @@ namespace CourseWork.ViewModels
         }
 
         [Command]
-        public void ValidateAndSave(RowValidationArgs args)
+        public void ValidateEmployeeRow(RowValidationArgs args)
         {
+            var item = (Employee)args.Item;
+            args.Result = GetEmployeeValidationErrorInfo(item);
+            if (args.Result == null)
+            {
+                if (args.IsNewItem)
+                    dbContext.Employees.Add(item);
+                dbContext.SaveChanges();
+            }
+        }
+
+        private static ValidationErrorInfo GetEmployeeValidationErrorInfo(Employee employee)
+        {
+            if (string.IsNullOrEmpty(employee.Name))
+                return new ValidationErrorInfo("Please fill Name");
+            if (string.IsNullOrEmpty(employee.Surname))
+                return new ValidationErrorInfo("Please fill Name");
+            if (string.IsNullOrEmpty(employee.PhoneNumber))
+                return new ValidationErrorInfo("Please fill Phone Number");
+            if (employee.Salary <= 0)
+                return new ValidationErrorInfo("Incorrect Salary");
+            if (employee.BirthDate > DateTime.Now)
+                return new ValidationErrorInfo("Invalid Birth Date");
+            return null;
+        }
+
+        [Command]
+        public void ValidateEmployeeRowDeletion(ValidateRowDeletionArgs args)
+        {
+            var item = (Employee)args.Items.Single();
+            dbContext.Employees.Remove(item);
             dbContext.SaveChanges();
+        }
+
+        [Command]
+        public void DataSourceRefresh(DataSourceRefreshArgs args)
+        {
+
         }
     }
 }
